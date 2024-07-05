c43187d8065b34b968e5b1b24a1c35c2f53d21e1
Andreas Hecht <a.hecht1@oth-aw.de>
2024-07-05 02:06:06 +0200

    Added DB, Added PUT endpoint for data

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,37 @@
-import sqlite3 from 'sqlite3'
-import { open } from 'sqlite'
-import express from 'express'
 import dotenv from 'dotenv'
+import sqlite3 from 'sqlite3'
+import express from 'express'
 
-dotenv.config();
+
+dotenv.config({path: '../.env'});
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
-export async function openDb () {
-    return open({
-        filename: '/database.db',
-        driver: sqlite3.Database
-    });
-}
-const db = new sqlite3.Database('./database.db', (err) => {
+const db = new sqlite3.Database('./database', (err) => {
     if (err) {
         console.error(err.message);
     }
-    console.log('Connected to the products database.');
+    console.log('Connected to the database.');
 });
 
-app.use(express.json());
-
-app.put('/api/insertPulseData/:heartRate/:rawInfrared/:oxygen', async (req, res) => {
-    const { heartRate, rawInfrared,oxygen } = req.body;
+app.put('/api/insertPulseData', function (req, res)
+{
+    const {heartRate, rawInfrared, oxygen} = req.body;
     const query = `
-    INSERT INTO pulseData (
-         timeStamp, heartRate, rawInfrared,oxygen
-    )
-    VALUES (time('now'), $1, $2, $3)`
+        INSERT INTO pulseData (timeStamp, heartRate, rawInfrared, oxygen)
+        VALUES (time ('now'), $1, $2, $3)`
     try {
         db.run(query, [heartRate, rawInfrared, oxygen]);
-        res.status(200).send({ heartRate, rawInfrared,oxygen });
+        res.status(200).send({heartRate, rawInfrared, oxygen});
     } catch (err) {
         console.error('Error executing query', err.message);
         res.status(500).send('Internal server error');
     }
 })
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+
+app.listen(port, function (err) {
+    if (err) console.log(err);
+    console.log(`Server listening on PORT ${port}`);
+});
