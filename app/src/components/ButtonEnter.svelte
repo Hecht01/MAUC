735690d36147b3ceccae26cf6341eca1d96cf9a2
Eva-Maria Maurer <e.maurer@oth-aw.de>
2024-07-05 15:31:26 +0200

    Update ButtonEnter.svelte

--- app/src/components/ButtonEnter.svelte
+++ app/src/components/ButtonEnter.svelte
@@ -3,33 +3,31 @@ import {Button} from "flowbite-svelte";
 
 export let username:string;
 
-async function put(data: {
-    'heartRate': string,
-    'rawInfrared': string,
-    'oxygen': string,
-    'username': string
-}) {
-    let url = "http://localhost:3000/insertPulseData";
-    // Awaiting fetch which contains method,
-    // headers and content-type and body
-    const response = fetch(url, {
+const data = {
+    heartRate: '123',
+    rawInfrared: '1235',
+    oxygen: '99',
+    username: 'Andy'
+}
+
+const url:string = "http://localhost:3000/insertPulseData"
+
+async function put(url:string, data:any)
+{
+    const response = await fetch(url, {
         method: 'PUT',
         headers: {
             'Content-type': 'application/json'
         },
-        //body: JSON.stringify(data),
-        body: JSON.parse(JSON.stringify(data))
+        body: JSON.stringify(data),
     });
-
-    console.log("Response: ", response )
-
-    // Awaiting response.json()
-    //const resData = await response.json();
-
-    // Return response data
-    //return resData;
+    const resdata = await response.json();
+    return resdata;
 }
+
 
 </script>
 
-<Button color="alternative" on:click = {() => put({'heartRate': "130", 'rawInfrared': "235", 'oxygen': "97", 'username': username})}>Enter</Button>
+<h2>{username}</h2>
+
+<Button color="alternative" on:click = {() => put(url, {heartRate: '130', rawInfrared: '235', oxygen: '97', username: username}).then(data => console.log(data))}>Enter</Button>
