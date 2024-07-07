58c353bc79f336e85126b86706d681f97584291a
Andreas Hecht <a.hecht1@oth-aw.de>
2024-07-07 15:16:15 +0200

    Maybe now?

--- app/src/components/PulseEffect.svelte
+++ app/src/components/PulseEffect.svelte
@@ -1,46 +1,11 @@
 <script lang="ts">
     import heart from "$lib/assets/heart.png"
-    import {spring} from "svelte/motion";
-    import type {Action} from "svelte/action";
+    import { fade, scale } from "svelte/transition";
 
     let animate = true;
     let checked = true;
-    type SpringInParams = {
-        scale: number
-        rotate: number
-    }
 
-    type SpringInAction = Action<HTMLElement, SpringInParams>
-
-    const springIn: SpringInAction = (node, params) => {
-        if (!params){
-            return
-        }
-        const {scale, rotate} = params;
-        const value = {rotate: 0, scale: 1};
-        const options = {stiffness: 0.1, damping: 0.6};
-
-        let transition = spring(value, options);
-
-        const unsubscribe = transition.subscribe(
-            ({rotate, scale}) => {
-                node.style.transform = `scale(${scale}) rotate(${rotate}deg)`
-            }
-        )
-
-        transition.set(({scale, rotate}))
-        return{
-            destroy: () => unsubscribe()
-        }
-    }
 </script>
-
-{#if animate}
-    <div
-    class = "image"
-    use:springIn={{scale:2, rotate: 90}}
-    />
-{/if}
 
 <style>
     main{
@@ -68,16 +33,12 @@
     </label>
 
     {#if checked}
-        <div class = "img_holder">
-            <label>
-                <input type="checkbox" bind:checked={animate}/>
-                Enable/ Disable Beat Animation
-            </label>
-
+        <div class = "img_holder" in:scale out:fade>
             <div class = "image" id = "image">
                 <img src ={heart} alt = "red heart symbol with a blue line that signifies the heartbeat"/>
             </div>
         </div>
+
     {:else}
         <p>Check to box to reenable the heartbeat.</p>
     {/if}
